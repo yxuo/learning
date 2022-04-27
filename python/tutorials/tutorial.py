17a22d4432c413313f18e5b7fa61784895eabf4d
Raphael Rivas <raphaelrivas@hotmail.com>
2022-04-27 13:32:19 -0300

    new tutorial in python: the basics

---
+++ python/tutorials/tutorial.py
@@ -0,0 +1,65 @@
+# aula 1
+
+# comentário de 1 linha
+
+'''
+Comentário de
+várias
+linhas
+'''
+
+print("hello world")
+
+# realiza operações matemáticas
+
+print(1+1) # 2      (soma)
+
+print(1-1) # 0      (subtração)
+
+print(2*3) # 6      (multiplicação)
+
+print(2**3) # 8     (potência)
+
+print(2/3) # 0.6_   (divisão)
+
+print(2//3) # 0     (divisão arredondado pra baixo)
+
+print(10%2) # 0      (resto)
+
+# juntar texto com número
+
+print("texto", 10) # texto 10                       (vírgula, espaço automático)
+
+print("texto %d"%10) # texto 10                     (estilo C)
+print("texto %d"%(10 + 1)) # texto 11               (operações matemáticas)
+print("texto %d e %d"%(10, 34)) # texto 10 e 34     (mais de uma variável)
+
+print("texto {}".format(10)) # texto 10             (format)
+
+# aspas simples é o mesmo que aspas duplas
+
+print('hello world')
+print("hello world")
+
+# declarar várias variáveis na mesma linha
+
+a, b, c, d, e = 1, 2, 3, 4, 5
+# a=1, b=2, c=3, d=4, e=5
+
+# operadores lógicos
+
+print(1 == 2) # False
+
+print(1 > 2) # False
+
+print(1 >= 1) # True
+print(1 >= 2) # False
+
+print(1 <= 1) # True
+print(1 <= 2) # True
+
+print(not 1 < 2) # False
+
+# oepradores relacionais
+
+print(not 1 == 1) # False
