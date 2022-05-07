9dff57387ddc9b66f19cf810abcd5c6f4418c527
Raphael Rivas <raphaelrivas@hotmail.com>
2022-05-07 00:54:11 -0300

    add echo examples

--- web/courses/guilherme_guanabara/php-basic/script.php
+++ web/courses/guilherme_guanabara/php-basic/script.php
@@ -3,5 +3,16 @@
 $boolTrue1 = (bool) true;
 $boolFalse1 = False;
 
-var_dump($boolTrue1); 
-var_dump($boolFalse1); 
+var_dump($boolTrue1);
+var_dump($boolFalse1);
+
+// 4
+
+$txt1 = "hello";
+$txt2 = "world";
+$num1 = 123;
+$num1 = 456;
+
+echo $txt1 . $txt2 . $num1 . $num2 . "\n";
+echo "{$txt1} {$txt2} {$num1} {$num2}\n";
+echo '$txt1 $txt2 $num1 $num2\n';
