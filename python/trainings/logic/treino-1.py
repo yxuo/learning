ffaf2a0112ad4192310dcd2f8fbbe755dbee1b70
Raphael Rivas <raphaelrivas@hotmail.com>
2022-05-17 15:10:55 -0300

    done q1.2-q2.2

--- python/trainings/logic/treino-1.py
+++ python/trainings/logic/treino-1.py
@@ -1,5 +1,38 @@
 # q1.1
-nums = [2,3,4,5,10,12]
-print("resultado = %d"%(sum(nums)/2))
+def q1_1():
+    nums = [2,3,4,5,10,12]
+    print("resultado = %d"%(sum(nums)/2))
 
-# q1.2
+# q1.2-5 OK
+
+#q2
+
+# q2.1 OK
+
+# import random
+# num = random.randint(0,9999) # ! não use ';'
+# num = str(num) # ! string() NÃO EXISTE
+# print(num)
+# print("unidade",num[-1])
+# if len(num) > 1:
+#     print("dezena",num[-2])
+# if len(num) > 2:
+#     print("centena",num[-3])
+# if len(num) > 3:
+#     print("milhar",num[-4])
+
+# q2.2 OK
+
+# a = int(input("insira num "))
+# b = int(input("insira num "))
+# c = int(input("insira real "))
+# print(2*a*(b/2))
+# print((3*a)+c)
+# print(pow(c,3))
+
+# q2.3
+
+# q2.4
+
+# q2.5
+
