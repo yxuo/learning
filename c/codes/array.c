76f3496c9bb41aa83eca1d5289e1c5691e238cb8
Raphael Rivas <raphaelrivas@hotmail.com>
2022-06-05 22:30:26 -0300

    finished array ptr

--- c/codes/array.c
+++ c/codes/array.c
@@ -2,138 +2,6 @@
 #include <stdlib.h> //alloc free
 #include <string.h> //memset, memcpy
 
-// ARRAY POINTER
-
-void print_array_ptr(int *arr, int size, char *end)
-{
-    if (end)
-        printf("%s", end);
-    for (int i = 0; i < size; i++)
-        printf("%i ", arr[i]);
-    printf("\n");
-}
-
-int *array_ptr_ini(int size)
-{
-    int *arr = (int *)malloc(sizeof(int) * size);
-    for (int i = 0; i < size; i++)
-        arr[i] = 0;
-    return arr;
-}
-
-void array_ptr_add(int *arr, int *size, int pos, int value)
-{
-    if (pos < 0)
-        pos = *size - 1;
-    *size += 1;
-    arr = (int *)realloc(arr, sizeof(int) * *size);
-    for (int i = *size - 1; i > pos; i--)
-        arr[i] = arr[i - 1];
-    arr[pos] = value;
-}
-
-void array_ptr_remove(int **arr, int *size, int pos)
-{
-    if (!pos)
-        pos = *size - 1;
-    *arr = (int *)realloc(arr, sizeof(int) * *--size);
-    for (int i = 0; i < *size - 1; i++)
-        arr[i] = arr[i + 1];
-}
-
-void array_ptr_fn()
-{
-    int size = 3;
-    int *arr = array_ptr_ini(size);
-
-    // memset
-    for (int i = 0; i < size; i++)
-        arr[i] = i+1;
-    print_array_ptr(arr, size, "  ");
-
-    // add first
-    array_ptr_add(arr, &size, 0, 4);
-    print_array_ptr(arr, size, "4+ ");
-
-    // add pos
-    array_ptr_add(arr, &size, 1, 5);
-    print_array_ptr(arr, size, "5+ ");
-
-    // add final
-    array_ptr_add(arr, &size, -1, 6);
-    print_array_ptr(arr, size, "6+ ");
-}
-
-void array_ptr()
-{
-    int size = 3;
-    int *arr = 0;
-
-    // create array
-    arr = (int *)malloc(sizeof(int) * size);
-
-    // memset
-    for (int i = 0; i < size; i++)
-        arr[i] = 0;
-
-    // set values
-    for (int i = 0; i < 3; i++)
-        arr[i] = i + 1;
-
-    // print list
-    printf("  \t");
-    for (int i = 0; i < size; i++)
-        printf("%i ", arr[i]);
-    printf("\n");
-
-    // insert first
-    size++;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    int pos = 0;
-    for (int i = size - 1; i > 0; i--)
-        arr[i] = arr[i - 1];
-    arr[pos] = 4;
-    print_array_ptr(arr, size, "+4\t");
-
-    // insert pos
-    size++;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    pos = 1;
-    for (int i = size - 1; i > pos; i--)
-        arr[i] = arr[i - 1];
-    arr[pos] = 5;
-    print_array_ptr(arr, size, "+5\t");
-
-    // insert last
-    size++;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    pos = size - 1;
-    arr[pos] = 6;
-    print_array_ptr(arr, size, "+6\t");
-
-    // delete first
-    for (int i = 0; i < size - 1; i++)
-        arr[i] = arr[i + 1];
-    size--;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    print_array_ptr(arr, size, "-4\t");
-
-    // delete pos
-    pos = 1;
-    for (int i = pos; i < size - 1; i++)
-        arr[i] = arr[i + 1];
-    size--;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    print_array_ptr(arr, size, "-1\t");
-
-    // delete last
-    size--;
-    arr = (int *)realloc(arr, sizeof(int) * size);
-    print_array_ptr(arr, size, "-6\t");
-}
-
-// TODO: ARRAY DYNAMIC
-
 void array_dynamic()
 {
     // code
@@ -281,6 +149,152 @@ void array_basic()
     size--;
     print_array_basic(arr, sizeof(arr) / sizeof(int), "-6\n");
 }
+
+// ARRAY POINTER
+
+void print_array_ptr(int *arr, int size, char *end)
+{
+    if (end)
+        printf("%s", end);
+    for (int i = 0; i < size; i++)
+        printf("%i ", arr[i]);
+    printf("\n");
+}
+
+int *array_ptr_ini(int size)
+{
+    int *arr = (int *)malloc(sizeof(int) * size);
+    for (int i = 0; i < size; i++)
+        arr[i] = 0;
+    return arr;
+}
+
+void array_ptr_add(int **arr, int *size, int pos, int value)
+{
+    *size += 1;
+    if (pos < 0)
+        pos = *size - 1;
+    (*arr) = (int *)realloc((*arr), sizeof(int) * *size);
+    for (int i = *size - 1; i > pos; i--)
+        (*arr)[i] = (*arr)[i - 1];
+    (*arr)[pos] = value;
+}
+
+void array_ptr_del(int **arr, int *size, int pos)
+{
+    if (pos < 0)
+        pos = *size - 1;
+    for (int i = pos; i < *size - 1; i++)
+        (*arr)[i] = (*arr)[i + 1];
+    *size -= 1;
+    (*arr) = (int *)realloc((*arr), sizeof(int) * *size);
+        // printf("%i = %i\n",(*arr)[i], (*arr)[i + 1]),
+}
+
+void array_ptr_fn()
+{
+    int size = 3;
+    int *arr = array_ptr_ini(size);
+
+    // memset
+    for (int i = 0; i < size; i++)
+        arr[i] = i+1;
+    print_array_ptr(arr, size, "   ");
+
+    // add first
+    array_ptr_add(&arr, &size, 0, 4);
+    print_array_ptr(arr, size, "+4\t");
+
+    // add pos
+    array_ptr_add(&arr, &size, 1, 5);
+    print_array_ptr(arr, size, "+5\t");
+
+    // add end
+    array_ptr_add(&arr, &size, -1, 6);
+    print_array_ptr(arr, size, "+6\t");
+
+    // del first
+    array_ptr_del(&arr, &size, 0);
+    print_array_ptr(arr, size, "-4\t");
+
+    // del pos
+    array_ptr_del(&arr, &size, 1);
+    print_array_ptr(arr, size, "-1\t");
+
+    // del end
+    array_ptr_del(&arr, &size, -1);
+    print_array_ptr(arr, size, "-6\t");
+}
+
+void array_ptr()
+{
+    int size = 3;
+    int *arr = 0;
+
+    // create array
+    arr = (int *)malloc(sizeof(int) * size);
+
+    // memset
+    for (int i = 0; i < size; i++)
+        arr[i] = 0;
+
+    // set values
+    for (int i = 0; i < 3; i++)
+        arr[i] = i + 1;
+
+    // print list
+    printf("  \t");
+    for (int i = 0; i < size; i++)
+        printf("%i ", arr[i]);
+    printf("\n");
+
+    // insert first
+    size++;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    int pos = 0;
+    for (int i = size - 1; i > 0; i--)
+        arr[i] = arr[i - 1];
+    arr[pos] = 4;
+    print_array_ptr(arr, size, "+4\t");
+
+    // insert pos
+    size++;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    pos = 1;
+    for (int i = size - 1; i > pos; i--)
+        arr[i] = arr[i - 1];
+    arr[pos] = 5;
+    print_array_ptr(arr, size, "+5\t");
+
+    // insert last
+    size++;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    pos = size - 1;
+    arr[pos] = 6;
+    print_array_ptr(arr, size, "+6\t");
+
+    // delete first
+    for (int i = 0; i < size - 1; i++)
+        arr[i] = arr[i + 1];
+    size--;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    print_array_ptr(arr, size, "-4\t");
+
+    // delete pos
+    pos = 1;
+    for (int i = pos; i < size - 1; i++)
+        arr[i] = arr[i + 1];
+    size--;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    print_array_ptr(arr, size, "-1\t");
+
+    // delete last
+    size--;
+    arr = (int *)realloc(arr, sizeof(int) * size);
+    print_array_ptr(arr, size, "-6\t");
+}
+
+// TODO: ARRAY DYNAMIC
 
 int main()
 {
