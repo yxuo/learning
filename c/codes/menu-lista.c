f908ca73608ed856ae7c419aad353e8f9279f50d
Raphael Rivas <raphaelrivas@hotmail.com>
2022-06-23 09:30:52 -0300

    new code: menu-lista

--- c/codes/menu-lista.c
+++ c/codes/menu-lista.c
@@ -1,9 +1,7 @@
 /**
- * @file listas_encadeadas_recursivo.c
- * @author Raphael Rivas (raphaelrivas@hotmail.com)
- * @brief Qualquer dúvida, me chame e verei se posso ajudar
- * @version 0.1
- * @date 2022-04-01
+ * @file menu-lista.c
+ * @version 0.2
+ * @date 2022-06-20
  *
  * @copyright Copyright (c) 2022
  *
@@ -53,44 +51,19 @@ void lista_adicionar_indice(No **no, int valor, int pos)
 
     // 2. percorrer até a posição, daí este elemento  pode apontar para o novo elemento
 
-    if (pos > 1 || (pos < 0 && (*no)->Prox))
+    if (pos > 0 || (pos < 0 && (*no)->Prox))
         lista_adicionar_indice(&(*no)->Prox, valor, pos - 1);
 
     // 3. adicionar nó no fim
     else
-    {
-        // No *novo = lista_novo_no(valor);
-        // novo->Prox = *no;
-        // *no = novo;
-        (*no)->Prox = lista_novo_no(valor);
-    }
-}
-
-void lista_adicionar_fim(No **no, int valor)
-{
-    // 1. se a lista tá vazia, adicionar o 1o elemento
-    if (*no == NULL)
-    {
-        *no = lista_novo_no(valor);
-        return;
-    }
-
-    // 2. percorrer até o último elemento, daí este último pode apontar para o novo elemento
-
-    if ((*no)->Prox != NULL) //                         chama função até o fim da lista
-        lista_adicionar_fim(&(*no)->Prox, valor);
-
-    // 3. adicionar nó no fim
-    else
         (*no)->Prox = lista_novo_no(valor);
 }
 
-// nada de recursivo para fazer aqui...
-void lista_adicionar_inicio(No **no, int valor)
+int lista_len(No *no)
 {
-    No *novo = lista_novo_no(valor);
-    novo->Prox = *no;
-    *no = novo;
+    if(!no)
+        return 0;
+    return lista_len(no->Prox) + 1;
 }
 
 void print_lista(No *no)
@@ -167,37 +140,11 @@ int lista_remover_valor(No **no, int valor)
     return 0;
 }
 
-void lista_remover_ultimo(No **no)
-{
-    // 1. se lsita é nula, cancela
-    if (*no == NULL)
-        return;
-
-    // 2. remove o último item
-    if ((*no)->Prox == NULL)
-    {
-        free(*no);
-        *no = NULL;
-        return;
-    }
-
-    // 3. enquanto não está no fim da lista
-    if ((*no)->Prox->Prox != NULL)
-        lista_remover_ultimo(&(*no)->Prox);
-
-    // 4. remover último nó e limpar Prox
-    else
-    {
-        free((*no)->Prox);
-        (*no)->Prox = NULL;
-    }
-}
-
 // mesma coisa, só muda o índice ao invés do valor
 int lista_remover_indice(No **no, int indice)
 {
     // 1. se lista é nula, cancela
-    if (*no == NULL || indice < 0)
+    if (*no == NULL)
         return 0;
 
     // há muitas maneiras de fazer esta função de forma recursiva;
@@ -208,8 +155,11 @@ int lista_remover_indice(No **no, int indice)
     // 2. remove caso indice foi atingido
 
     // 2.1.a. caso seja o último nó
-    if ((*no)->Prox == NULL && indice == 0)
+    if ((*no)->Prox == NULL)
     {
+        // envita deletar o último elemento caso o índice esteja fora
+        if(indice > 0)
+            return 0;
         free(*no);
         *no = NULL;
 
@@ -269,36 +219,49 @@ int main()
                "2. Remover valor\n"
                "3. Remover indice\n"
                "4. Imprimir lista\n"
-               "5. Ajuda\n"
-               "6. Sair\n"
+               "5. Comprimento da lista\n"
+               "6. Ajuda\n"
+               "7. Sair\n"
                ">> ");
         scanf("%i", &entrada);
 
         switch (entrada)
         {
-        case 6:
+        case 7:
             return 0;
             break;
-        case 5:
+        case 6:
             printf(
                 "Lista:\n"
                 ".Esta e uma lista unicamente encadeada, apontando sempre para o elemento posterior\n"
                 "Indices:\n"
-                ".Se voce usar um indice fora da lista, o indice sera o ultimo elemento\n"
+                ".Se voce usar um indice fora da lista, nada acontece\n"
+                ".Se voce usar um indice negativo, o indice sera o ultimo elemento\n"
                 "Remover:\n"
-                ".Se voce remover um item de uma lista vazia, nada acontecera\n\n"
+                ".Se voce remover um item de uma lista vazia, um aviso sera dado\n\n"
                 );
+            break;
+        case 5:
+            printf("Comprimento: %i\n",lista_len(lista));
             break;
         case 4:
             print_listaln(lista);
             break;
         case 3:
-            printf("Selecione o indice do item a remover: ");
+            if(!lista_len(lista)){
+                printf("Remover indice: Lista vazia!\n");
+                break;
+            }
+            printf("Selecione o indice do item a remover ([-1] para remover no final): ");
             scanf("%i", &indice);
             lista_remover_indice(&lista, indice);
             print_listaln(lista);
             break;
         case 2:
+            if(!lista_len(lista)){
+                printf("Remover valor: Lista vazia!\n");
+                break;
+            }
             printf("Selecione o valor do item a remover: ");
             scanf("%i", &valor);
             lista_remover_valor(&lista, valor);
@@ -307,7 +270,7 @@ int main()
         case 1:
             printf("Selecione o valor do novo item: ");
             scanf("%i", &valor);
-            printf("Selecione o 5ndice a adicionar ([-1] para adicionar no final): ");
+            printf("Selecione o indice a adicionar ([-1] para adicionar no final): ");
             scanf("%i", &indice);
             lista_adicionar_indice(&lista, valor, indice);
             print_listaln(lista);
@@ -317,31 +280,4 @@ int main()
             printf("comando invalido!\n");
         }
     }
-    No *lista_invertida = NULL;
-
-    printf("1. adicionar itens no inicio da lista\n");
-    for (int i = 0; i < 5; i++)
-        lista_adicionar_indice(&lista_invertida, i, 0);
-    print_listaln(lista_invertida);
-
-    printf("2. adicionar itens no fim da lista\n");
-    for (int i = 0; i < 5; i++)
-        lista_adicionar_indice(&lista, i, -1);
-    print_listaln(lista);
-
-    printf("2. adicionar item no indice [3]\n");
-    lista_adicionar_indice(&lista, 10, 3);
-    print_listaln(lista);
-
-    printf("3. remover ultimo item\n");
-    lista_remover_ultimo(&lista);
-    print_listaln(lista);
-
-    printf("4. remover item no indice [2] = 2\n");
-    lista_remover_indice(&lista, 2);
-    print_listaln(lista);
-
-    printf("5. remover item com valor (1)\n");
-    lista_remover_valor(&lista, 1);
-    print_listaln(lista);
 }
