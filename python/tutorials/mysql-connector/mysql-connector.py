682d52a94aaef32413e258c299cbb458160d4c1b
Raphael Rivas <raphaelrivas@hotmail.com>
2022-06-27 01:47:00 -0300

    minor changes

--- python/tutorials/mysql-connector/mysql-connector.py
+++ python/tutorials/mysql-connector/mysql-connector.py
@@ -36,20 +36,50 @@ REQUISITOS:
 '''
 
 import mysql.connector
+import os
 
-# cria objeto de conexão SQL
 conexao = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = ''
+    host='localhost',
+    user='root',
+    password=''
 )
 
-# cursor é a linha selecionada no BD
 cursor = conexao.cursor()
 
-# criar BD
-
 cursor.execute('CREATE DATABASE IF NOT EXISTS aula8')
+cursor.fetchall()
+
+
+# TEMP
+cursor.execute("show columns from cadastro like 'time';")
+if not cursor.fetchall():
+    cursor.execute("ALTER TABLE 'cadastro' ADD 'time_id' INT NOT NULL AFTER 'senha';")
+    conexao.commit()
+    cursor.execute("ALTER TABLE 'cadastro' ADD CONSTRAINT 'time_fk' FOREIGN KEY ('time_id') REFERENCES 'times'('id') ON DELETE NO ACTION ON UPDATE NO ACTION;")
+    conexao.commit()
+    # ALTER TABLE `cadastro` ADD CONSTRAINT `time_fk` FOREIGN KEY (`time_id`) REFERENCES `times`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION; 
+
+    # alter table  'cadastro' add constraint 'time_fk' foreign key ('time_id') references 'times'('id') on delect no action;
+    
+    
+# IF EXISTS( SELECT time FROM cadastro)THEN select * from cadastro END IF;
+
+# if not exists(select * from information_schema.tables where table_schema = 'TheSchema') then 
+cursor.execute("\
+                IF NOT EXISTS( \
+                    SELECT * \
+                    FROM INFORMATION_SCHEMA.TABLES \
+                    WHERE TABLE_SCHEMA = 'TheSchema' \
+                    AND  TABLE_NAME = 'TheTable')) \
+                ) THEN \
+                    ALTER TABLE `TableName` ADD `ColumnName` int(1) NOT NULL default '0';\
+                END IF;\
+")
+# select * from information_schema.table_constraints where constraint_schema = 'cadastro'
+# ALTER TABLE `cadastro` ADD `ColumnName` int(1) NOT NULL default '0';
+
+os._exit(0)
+# TEMP
 
 # percorrer (e descartar) todas as linhas do resultado
 
@@ -67,19 +97,22 @@ print(retorno)
 
 # entrar no BD
 
-cursor.execute("USE aula8;")
+cursor.execute("USE aula8")
 
 # criar tabela
 
-cursor.execute(" \
-CREATE TABLE IF NOT EXISTS aluno( \
-    matricula int PRIMARY KEY AUTO_INCREMENT, \
-    nome varchar(40), \
-    idade int(3), \
-    email varchar(30) \
-); \
-")
-cursor.fetchall()
+# cursor.execute(" \
+# CREATE TABLE IF NOT EXISTS aluno( \
+#     matricula int PRIMARY KEY AUTO_INCREMENT, \
+#     nome varchar(40), \
+#     idade int(3), \
+#     email varchar(30) \
+# ) \
+# ")
+cursor.execute(
+    "create table if not exists cadastro(usuario varchar(20) primary key, senha varchar(20))")
+# cursor.fetchall()
+# os._exit(0)
 
 # mostrar tabelas do BD selecionado
 cursor.execute("SHOW TABLES;")
@@ -87,10 +120,10 @@ cursor.fetchall()
 
 # mostrar atributos da tabela
 
-cursor.execute("DESC aluno;") #DESC ou DESCRIBE
+cursor.execute("DESC aluno;")  # DESC ou DESCRIBE
 cursor.fetchall()
 
-#inserir dados
+# inserir dados
 
 # string esqueleto para executar SQL
 '''
